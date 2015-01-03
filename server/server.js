1b7795e6119855880c43ea961b1f67fd83efdbe6
Thr1ve <Buhler@gordons-mbp.attlocal.net>
2015-01-03 17:46:11 -0500

    moved moonboots to routes.js bootfile

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ var fixPath = function (pathString) {
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname);
 
+/////////////////////////////////
 // -- Mount static files here--
 // All static middleware should be registered at the end, as all requests
 // passing the static middleware are hitting the file system
@@ -23,103 +24,7 @@ var landing = require('path').resolve(__dirname, '../client/landing');
 
 app.use(loopback.static(landing));
 
-var dispenseManager = new Moonboots({
-    server: app,
-    appPath : '/dispenseManager*',
-    moonboots: {
-        jsFileName: 'DispenseManager',
-        cssFileName: 'DispenseManager',
-        main: require('path')
-            .resolve(__dirname, '../client/dispenseManager/client/app.js'),
-        // developmentMode: true,
-        libraries: [
-        ],
-        stylesheets: [
-            fixPath('client/dispenseManager/public/css/bootstrap.css'),
-            fixPath('client/dispenseManager/public/css/app.css')
-        ],
-        browserify: {
-            // debug: true
-        },
-        beforeBuildJS: function () {
-            // This re-builds our template files from jade each time the app's main
-            // js file is requested. Which means you can seamlessly change jade and
-            // refresh in your browser to get new templates.
-
-            // if (config.isDev) {
-                templatizer(fixPath('client/dispenseManager/templates'),
-                    fixPath('client/dispenseManager/client/templates.js'));
-            // }
-        },
-        beforeBuildCSS: function (done) {
-            // This re-builds css from stylus each time the app's main
-            // css file is requested. Which means you can seamlessly change stylus files
-            // and see new styles on refresh.
-
-            // if (config.isDev) {
-                stylizer({
-                    infile: fixPath('client/dispenseManager/public/css/app.styl'),
-                    outfile: fixPath('client/dispenseManager/public/css/app.css'),
-                    // development: true
-                    development: false
-                }, done);
-            // } else {
-                // done();
-            // }
-        }
-    }
-});
-
-var dispenseApp = new Moonboots({
-    server: app,
-    appPath : '/dispenseApp*',
-    moonboots: {
-        jsFileName: 'DispenseApp',
-        cssFileName: 'DispenseApp',
-        main: require('path')
-            .resolve(__dirname, '../client/dispenseApp/client/app.js'),
-        // developmentMode: true,
-        libraries: [
-        ],
-        stylesheets: [
-            fixPath('client/dispenseApp/public/css/bootstrap.css'),
-            fixPath('client/dispenseApp/public/css/app.css')
-        ],
-        browserify: {
-            // debug: true
-        },
-        beforeBuildJS: function () {
-            // This re-builds our template files from jade each time the app's main
-            // js file is requested. Which means you can seamlessly change jade and
-            // refresh in your browser to get new templates.
-
-            // if (config.isDev) {
-                templatizer(fixPath('client/dispenseApp/templates'),
-                    fixPath('client/dispenseApp/client/templates.js'));
-            // }
-        },
-        beforeBuildCSS: function (done) {
-            // This re-builds css from stylus each time the app's main
-            // css file is requested. Which means you can seamlessly change stylus files
-            // and see new styles on refresh.
-
-            // if (config.isDev) {
-                stylizer({
-                    infile: fixPath('client/dispenseApp/public/css/app.styl'),
-                    outfile: fixPath('client/dispenseApp/public/css/app.css'),
-                    // development: true
-                    development: false
-                }, done);
-            // } else {
-                // done();
-            // }
-        }
-    }
-});
-
-
-
-
+////////////////////////////////
 
 app.start = function() {
   // start the web server
