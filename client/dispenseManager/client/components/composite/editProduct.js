a76bff65cd2d22793b4f4de147ceffadaa795b4f
Thr1ve <fict1on.buhler@gmail.com>
2015-04-17 07:58:21 -0400

    update
    update

---
+++ client/dispenseManager/client/components/composite/editProduct.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var EditProduct = React.createClass({
+
+    render: function() {
+        return (
+            <div />
+        );
+    }
+
+});
+
+module.exports = EditProduct;
