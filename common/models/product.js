8045e3e5c7692a65ec6e58c405cb76ddb736823c
Thr1ve <fict1on.buhler@gmail.com>
2015-04-20 16:02:51 -0400

    update

--- common/models/product.js
+++ common/models/product.js
@@ -4,20 +4,20 @@ var app = require('../../server/server');
 module.exports = function(Product) {
 
 
-    Product.observe('before save', function(modelInstance, next) {
+    // Product.observe('before save', function(modelInstance, next) {
 
-        var products = app.datasources.mydb.models.product;
+    //     var products = app.datasources.mydb.models.product;
 
-        if(!modelInstance.instance.productId){
-            products.count(function(err,n){
-                modelInstance.instance.productId = n+1;
-                next();
-            });
-        }
-        else{
-            next();
-        }
-    });
+    //     if(!modelInstance.instance.productId){
+    //         products.count(function(err,n){
+    //             modelInstance.instance.productId = n+1;
+    //             next();
+    //         });
+    //     }
+    //     else{
+    //         next();
+    //     }
+    // });
 };
 
 
