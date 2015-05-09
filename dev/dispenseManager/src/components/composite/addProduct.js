240bc8ce4384d52fde077fa4e0865bddce011045
Thr1ve <fict1on.buhler@gmail.com>
2015-05-09 15:32:40 -0400

    added snackbar notification to addProduct

--- dev/dispenseManager/src/components/composite/addProduct.js
+++ dev/dispenseManager/src/components/composite/addProduct.js
@@ -2,12 +2,13 @@ import React from 'react'
 import app from 'ampersand-app'
 
 import Mui from 'material-ui'
-let { TextField, FlatButton, Paper } = Mui
+let { TextField, FlatButton, Paper, Snackbar } = Mui
 
 let addProduct = React.createClass({
 
     handleSubmit(e){
         e.preventDefault()
+        let self = this;
         let title = this.refs.title.getValue()
         let isbn13= this.refs.isbn13.getValue()
         let contact= this.refs.contact.getValue()
@@ -15,14 +16,17 @@ let addProduct = React.createClass({
             title   : title,
             isbn13  : isbn13
         }
+
         app.products.create( data, {
             wait: true, 
             success:function( model, resp ) {
-                console.log('Edit Successfull')
-                console.log(model)
-                console.log(resp)
+                self.success();
             }
         })
+    },
+
+    success() {
+        this.refs.snackbar.show()
     },
 
     getInitialState() {
@@ -47,35 +51,40 @@ let addProduct = React.createClass({
             padding:20
         }
         return (
-            <Paper zDepth={3} style={{width:'85%', marginRight:'auto', marginLeft:'auto'}}>
-                <form style = {formStyle} onSubmit={this.handleSubmit}>
-                    <div style={textFieldStyle}>
-                        <TextField 
-                            type='text' 
-                            name='title'          
-                            ref='title'          
-                            floatingLabelText='Title'            
-                            onChange={this.handleChange}/>
-                    </div>
-                    <div style={textFieldStyle}>
-                        <TextField
-                            type='text'
-                            name='isbn13'
-                            ref='isbn13'
-                            floatingLabelText='ISBN'
-                            onChange={this.handleChange}/>
-                    </div>
-                    <div style={textFieldStyle}>
-                        <TextField
-                            type='text'
-                            name='contact'
-                            ref='contact'
-                            floatingLabelText='Contact'
-                            onChange={this.handleChange}/>
-                    </div>
-                    <FlatButton label='Submit'/>
-                </form>
-            </Paper>
+            <div>
+                <Paper zDepth={3} style={{width:'85%', marginRight:'auto', marginLeft:'auto'}}>
+                    <form style = {formStyle} onSubmit={this.handleSubmit}>
+                        <div style={textFieldStyle}>
+                            <TextField 
+                                type='text' 
+                                name='title'          
+                                ref='title'          
+                                floatingLabelText='Title'            
+                                onChange={this.handleChange}/>
+                        </div>
+                        <div style={textFieldStyle}>
+                            <TextField
+                                type='text'
+                                name='isbn13'
+                                ref='isbn13'
+                                floatingLabelText='ISBN'
+                                onChange={this.handleChange}/>
+                        </div>
+                        <div style={textFieldStyle}>
+                            <TextField
+                                type='text'
+                                name='contact'
+                                ref='contact'
+                                floatingLabelText='Contact'
+                                onChange={this.handleChange}/>
+                        </div>
+                        <FlatButton label='Submit'/>
+                    </form>
+                </Paper>
+                <Snackbar
+                  ref="snackbar"
+                  message="Product Added!"/>
+            </div>
         )
     }
 
