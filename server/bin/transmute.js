7ea9f6cb2db229116f6d9a2845cb46cf84409b31
Thr1ve <fict1on.buhler@gmail.com>
2015-05-18 11:37:14 -0400

    update

--- server/bin/transmute.js
+++ server/bin/transmute.js
@@ -2,20 +2,32 @@ var loopback = require('loopback');
 
 //replace this with regcodes server details
 var dataSource = loopback.createDataSource('mssql', {
-	"host": "localhost",
+	// "host": "10.8.2.114",
+    "host": "localhost",
     "port": 1433,
-    "database": "testing",
-    "password": "test",
-    "user": "test"
+    "database": "RegCodes",
+    "password": "loopback",
+    "user": "loopback"
 });
 
 dataSource.discoverModelDefinitions(function(err, models){
-	console.log(models);
+	// console.log(models);
 	models.forEach(function (def) {
     // def.name ~ the model name
-    dataSource.discoverSchema(def.name, null, function (err, schema) {
-      console.log(schema);
-    });
+    console.log(def.name);
+    dataSource.discoverAndBuildModels(def.name , {owner:'dbo'}, function(err, nModels){
+        if(err) {
+            console.log(err)
+        }
+        // console.log(nModels);
+        // nModels.find(function(err, model){
+        //     console.log(model);
+        // })
+    })
+    // dataSource.discoverSchema(def.name, null, function (err, schema) {
+    //   console.log(schema);
+    // });
+    dataSource.disconnect();
   });
 });
 
