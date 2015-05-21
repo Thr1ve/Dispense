b7e60fe62ba746044f3c2bf43f11cd473f54ab26
Thr1ve <fict1on.buhler@gmail.com>
2015-05-21 14:00:23 -0400

    redirect api pointer to work pc for testing; minor cleanup

--- dev/dispenseApp/src/models/apiPointer.js
+++ dev/dispenseApp/src/models/apiPointer.js
@@ -5,7 +5,8 @@ module.exports = AmpCollection.extend({
     
     // pointer: 'http://192.168.1.85/api/',
     // pointer: 'http://thrive.geekgalaxy.com/api/',
-    pointer: 'http://localhost:8080/api/',
+    // pointer: 'http://localhost:8080/api/',
+    pointer: 'http://10.8.2.114:8080/api/',
 
     initialize : function() {
         this.url = this.pointer + this.location;
