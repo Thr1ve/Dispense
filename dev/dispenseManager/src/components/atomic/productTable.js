fc57f3b4bfbadabde5e092a9de06287869ffb7d4
Thr1ve <fict1on.buhler@gmail.com>
2015-07-14 11:30:34 -0400

    add mousetrap ; update productTable

--- dev/dispenseManager/src/components/atomic/productTable.js
+++ dev/dispenseManager/src/components/atomic/productTable.js
@@ -1,32 +1,42 @@
 import React from "react"
-// import Router from "react-router"
 import mui from "material-ui"
+import app from "ampersand-app"
+import KeyBindings from "react-side-effect-mousetrap"
+
 let { Menu } = mui
 
-let ProductTable = React.createClass({
+var ProductTable = React.createClass({
 
   contextTypes: {
     router: React.PropTypes.func
   },
 
-  render() {
-    let rows = []
+  _onItemClick: function(e, key, menuItem) {
+    var { router } = this.context
+    router.transitionTo("requestCode", { productId: menuItem.payload})
+  },
+
+  render: function() {
+    var rows = []
 
     this.props.products.forEach(function(product) {
       rows.push({payload: product.productId, text: product.title, data: product.isbn13})
-    }.bind(this))
+    })
 
     return (
-      <div>
-        <Menu menuItems={rows} onItemClick={this._onItemClick}/>
-      </div>
+      <KeyBindings keyMap={{
+        "esc": (e) => {
+          e.preventDefault()
+          app.trigger("clearText")
+        }
+      }}>
+        <div>
+          <Menu menuItems={rows} onItemClick={this._onItemClick}/>
+        </div>
+      </KeyBindings>
     )
-  },
-
-  _onItemClick(e, key, menuItem) {
-    let { router } = this.context
-    router.transitionTo("product", { productId: menuItem.payload})
   }
 })
+
 
 module.exports = ProductTable
