d978e059d3c306c30b21bf6d981b317f35a6dbe7
Thr1ve <fict1on.buhler@gmail.com>
2015-07-15 12:01:29 -0400

    fix api route for dispenseManager

--- dev/dispenseManager/src/components/atomic/productTable.js
+++ dev/dispenseManager/src/components/atomic/productTable.js
@@ -13,7 +13,7 @@ var ProductTable = React.createClass({
 
   _onItemClick: function(e, key, menuItem) {
     var { router } = this.context
-    router.transitionTo("requestCode", { productId: menuItem.payload})
+    router.transitionTo("product", { productId: menuItem.payload})
   },
 
   render: function() {
