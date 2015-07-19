aefba08ec254cda8526df7b7a790cece13f86df7
Thr1ve <fict1on.buhler@gmail.com>
2015-07-18 21:20:14 -0400

    remove front-end

--- dev/OLD_dispenseManager/client/pages/altSearch.js
+++
@@ -1,9 +0,0 @@
-//placeholder file for second filter view
-var PageView = require('./base');
-var templates = require('../templates');
-
-
-module.exports = PageView.extend({
-    pageTitle: 'browse',
-    template: templates.pages.altSearch
-});
