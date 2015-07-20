82d43a9bb1e0f7c79dec8e8f10f514d38444bfa4
Thr1ve <fict1on.buhler@gmail.com>
2015-07-20 11:37:02 -0400

    add changelog component and md file

---
+++ dev/dispenseApp/src/components/pages/changelog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+let Changelog = React.createClass({
+  render () {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+})
+
+module.exports = Changelog
