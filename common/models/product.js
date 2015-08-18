02afdb8e9280a95629c2c5788de42cc1ed8c59f9
Thr1ve <fict1on.buhler@gmail.com>
2015-08-17 22:14:06 -0400

    Revert "log productId and length"
    This reverts commit 73f253f4c5e95f5b679c2fa139711cbddcc5864b.

--- common/models/product.js
+++ common/models/product.js
@@ -1,17 +1,14 @@
-var app = require('../../server/server')
+// var app = require("../../server/server")
 
-module.exports = function (Product) {
 
-  Product.observe('before save', function (modelInstance, next) {
+module.exports = function(Product) {
 
-    var products = app.datasources.mydb.models.product
-    var prodId = modelInstance.productId
-    // var contacts = app.datasources.mydb.models.contact
-    if (!prodId) {
-      // console.log(modelInstance)
-      console.log(products)
-      console.log(products.length)
-    }
-    next()
-  })
+
+    // Product.observe("before save", function(modelInstance, next) {
+        // var products = app.datasources.mydb.models.product;
+        // var contacts = app.datasources.mydb.models.contact;
+        // var prodId   = modelInstance.productId;
+        // console.log(modelInstance)
+        // next()
+    // })
 }
