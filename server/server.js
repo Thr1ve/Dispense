b4ef1e376a485e6969ea1347e4a0d33ad6f755b9
Thr1ve <fict1on.buhler@gmail.com>
2015-08-26 18:08:34 -0400

    Add rethink and socket.io connection to server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ var boot = require('loopback-boot')
 var path = require('path')
 var app = module.exports = loopback()
 
+var dataSources = require('./datasources.json')
+var db = dataSources.rethinkdb
+var r = require('rethinkdb')
+
 boot(app, __dirname)
 
 // ///////////////////////////////
@@ -11,7 +15,7 @@ boot(app, __dirname)
 // passing the static middleware are hitting the file system
 // Example:
 // app.use(loopback.static(path.resolve(__dirname, '../client')))
-
+//
 var apps = path.resolve(__dirname, '../apps')
 var dispenseApp = path.resolve(__dirname, '../apps/dispenseApp')
 var dispenseManager = path.resolve(__dirname, '../apps/dispenseManager')
@@ -26,7 +30,8 @@ app.get('/dispenseManager*', function (req, res) {
   res.sendFile(dispenseManager + '/index.html')
 })
 
-// //////////////////////////////
+//
+// ///////////////////////////////
 
 app.start = function () {
   return app.listen(function () {
@@ -37,5 +42,26 @@ app.start = function () {
 
 // start the server if `$ node server.js`
 if (require.main === module) {
-  app.start()
+  app.io = require('socket.io')(app.start())
+
+  app.io.on('connection', function (socket) {
+
+    r.connect({
+      host: db.host,
+      port: db.port
+    }, function (err, conn) {
+      if (err) {throw err}
+
+      conn.use('dispense')
+
+      r.table('product').changes().run(conn)
+        .then(function (cursor) {
+          cursor.each(function (err, row) {
+            if (err) {throw err}
+            socket.emit('countUpdate', row)
+          })
+        })
+    })
+
+  })
 }
