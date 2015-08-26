6d89081f7e0b1725d6ebeec46eb4762183a6c0b5
Thr1ve <fict1on.buhler@gmail.com>
2015-08-26 18:13:37 -0400

    setState when there is a change in a product

--- dev/dispenseApp/pages/mainSearch.js
+++ dev/dispenseApp/pages/mainSearch.js
@@ -11,6 +11,25 @@ var MainSearch = React.createClass({
     router: React.PropTypes.func
   },
 
+  getInitialState () {
+    return {
+      products: {}
+    }
+  },
+
+  componentDidMount () {
+    app.products.on('change', (model, val) => {
+      // currently useless...I think I should pass down the products as state instead
+      // of referencing app.products in deeper components...
+      // this means possibly restructuring where the filtered array resides
+      this.setState({products: app.products})
+    })
+  },
+
+  componentWillUnmount () {
+    app.off('all')
+  },
+
   render () {
     return (
       <div>
