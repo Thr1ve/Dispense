d74c0db8b44217a5900ba95b611cb69d98ef7cd9
Thr1ve <fict1on.buhler@gmail.com>
2015-09-17 18:08:52 -0400

    add socket.io

--- dev/dispenseManager/app.js
+++ dev/dispenseManager/app.js
@@ -42,17 +42,37 @@ import Products from './../models/products'
 import Contacts from './../models/contacts'
 import Code from './../models/usedCode-collection'
 
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
 var app = App.extend({
   init () {
     // LOL SECURITY !1!!!1!!!!>
     // let allowed = window.prompt('')
     // if(allowed==='admin'){
 
+    var self = this
+
+    var io = require('socket.io-client')
+    var socket = io('http://localhost:5000')
+    // var socket = io('http://192.168.1.85')
+
+    this.socket = socket
+
     this.products = new Products()
     this.contacts = new Contacts()
     this.newCode = new Code()
 
     this.filterText = ''
+
+    socket.on('connect', function () {
+      socket.on('countUpdate', function (data) {
+        // console.log('Count Update Received! \n', data)
+        // console.log('nCodes for this product was ' + self.products.get(data.old_val.productId).nCodes)
+        self.products.get(data.old_val.productId).set('nCodes', data.new_val.nCodes)
+        // console.log('nCodes for this product is now ' + self.products.get(data.old_val.productId).nCodes)
+      })
+    })
 
     // Attach to window for easier debugging
     window.app = this
