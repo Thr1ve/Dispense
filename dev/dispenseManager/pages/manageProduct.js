e2608440ec456991b83b946377eaea10efda205a
Thr1ve <fict1on.buhler@gmail.com>
2015-10-06 11:38:31 -0400

    update dispenseManger pages

--- dev/dispenseManager/pages/manageProduct.js
+++ dev/dispenseManager/pages/manageProduct.js
@@ -1,47 +1,37 @@
-import app from 'ampersand-app'
 import React from 'react'
 import Router from 'react-router'
 let { RouteHandler } = Router
 
 import Mui from 'material-ui'
-var { FlatButton } = Mui
+let { FlatButton } = Mui
 
-// var log = require('bows')('manageProduct.js')
+import {r, DefaultMixin, QueryRequest} from 'react-rethinkdb'
 
 let ManageProduct = React.createClass({
+
+  mixins: [DefaultMixin],
 
   contextTypes: {
     router: React.PropTypes.func
   },
 
-  getInitialState () {
-    return {
-      productId: this.props.params.productId,
-      contact: null,
-      product: null
-    }
-  },
+  observe (props, state) {
 
-  componentDidMount () {
-    let self = this
-    app.contacts.getOrFetch(self.props.params.productId,
-      {all: true},
-      function (err, contact) {
-        if (err) {
-          console.error('contact not found', err)
-        }
-        self.setState({contact: contact})
-      }
-    )
-    app.products.getOrFetch(self.props.params.productId,
-      {all: true},
-      function (err, model) {
-        if (err) {
-          console.error('model not found', err)
-        }
-        self.setState({product: model})
-      }
-    )
+    // Turn our route into a number ( can't query with string )
+    let prodId = parseInt(this.context.router.getCurrentParams().productId, 10)
+
+    // Get the product from the server
+    return {
+      product: new QueryRequest({
+        query: r.table('products').filter({productId: prodId}),
+        initial: []
+      }),
+    // Get the contact from the server
+      contact: new QueryRequest({
+        query: r.table('contacts').filter({productId: prodId}),
+        initial: []
+      })
+    }
   },
 
   toEditProduct () {
@@ -58,13 +48,24 @@ let ManageProduct = React.createClass({
 
   render () {
 
+    let data = {}
+
     let buttonStyle = {
       height: '50',
       opacity: '0.9',
       float: 'right'
     }
 
-    if (this.state.product && this.state.contact) {
+    if (this.data.product && this.data.product._value.length > 0) {
+      data.product = this.data.product._value[0]
+      data.contact = this.data.contact._value[0]
+    } else {
+      data.product = {}
+      data.contact = {}
+    }
+
+    // if we don't have the product yet, nothing but "loading product" should be rendered
+    if (data.product && data.contact) {
       return (
         <div>
           <div style={{position: 'fixed', top: '0', left: '0', zIndex: '9'}} >
@@ -77,7 +78,7 @@ let ManageProduct = React.createClass({
               onClick={this.toAddCodes}
               secondary={true} />
           </div>
-          <RouteHandler {...this.state}/>
+          <RouteHandler {...data}/>
         </div>
       )
     } else {
