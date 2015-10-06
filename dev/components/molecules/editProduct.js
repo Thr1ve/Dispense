58a004c95b6c9fc38320692c3f4ebb5eaf739a60
Thr1ve <fict1on.buhler@gmail.com>
2015-10-06 11:39:33 -0400

    fix edit product; WORKING

--- dev/components/molecules/editProduct.js
+++ dev/components/molecules/editProduct.js
@@ -4,8 +4,6 @@ import EditProductForm from '../atoms/editProductForm.js'
 import Mui from 'material-ui'
 let { Paper, Snackbar } = Mui
 
-// var log = require('bows')('editProduct.js');
-
 let EditProduct = React.createClass({
 
   success () {
@@ -13,14 +11,16 @@ let EditProduct = React.createClass({
   },
 
   render () {
-    let { isbn13, title } = this.props.product
+    let title = this.props.product.title
+    let isbn13 = this.props.product.isbn13
+
     return (
       <div>
         <Paper zDepth={2} style={{width: '95%', marginRight: 'auto', marginLeft: 'auto'}}>
           <h2 className='mui-font-style-headline' style={{textAlign: 'center'}}>Edit: {title}</h2>
           <h4 style={{textAlign: 'center'}}>{isbn13}</h4>
         </Paper>
-        <EditProductForm {...this.props} success={this.success} />
+        <EditProductForm product={this.props.product} contact={this.props.contact} success={this.success} />
         <Snackbar
           ref='snackbar'
           message='Edit Successful'/>
